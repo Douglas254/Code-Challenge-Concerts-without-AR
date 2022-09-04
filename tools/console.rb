2a781b1714d1bf677cd81dc83bfb311d31632676
Douglas Obara <douglasobara97@gmail.com>
2022-09-04 15:09:20 +0300

    add bands instances

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,8 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+mugithi=Band.new("Mugithi","Kikuyu")
+sungusia=Band.new("Sungusia","Kisii")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
