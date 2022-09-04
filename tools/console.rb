0fece59f8cd0c76c7621daafb80b66f9bafb9c58
Douglas Obara <douglasobara97@gmail.com>
2022-09-04 15:28:17 +0300

    add concerts instances

--- tools/console.rb
+++ tools/console.rb
@@ -12,5 +12,7 @@ sungusia=Band.new("Sungusia","Kisii")
 event_place=Venue.new("Urban Brew Studios","Kisumu")
 dagoz_bar=Venue.new("Sound Africa","Nairobi")
 
+mashup=Concert.new('18th',mugithi,event_place)
+jams=Concert.new('12th',sungusia,dagoz_bar)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
